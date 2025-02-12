8f839156fc5bbdce8219b9ea48bb98e086a5e3f2
Felix Becker <becker@sitepark.com>
2025-02-12 17:50:19 +0100

    feat: added api endpoint /api/ical/search/{query}

--- src/Controller/ICalController.php
+++ src/Controller/ICalController.php
@@ -12,19 +12,29 @@ use Atoolo\Resource\ResourceChannel;
 use Atoolo\Resource\ResourceLanguage;
 use Atoolo\Resource\ResourceLoader;
 use Atoolo\Resource\ResourceLocation;
+use Atoolo\Search\Dto\Search\Query\SearchQuery;
+use Atoolo\Search\Search;
 use JsonException;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
-final class ICalController extends AbstractController
+final class ICalController extends AbstractController implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     public function __construct(
         private readonly ResourceChannel $channel,
         private readonly ResourceLoader $loader,
         private readonly ICalFactory $iCalFactory,
+        private readonly Search $search,
+        private readonly SerializerInterface $serializer,
     ) {}
 
     /**
@@ -61,14 +71,68 @@ final class ICalController extends AbstractController
         return $this->createICalResponseByLocation($resourceLocation);
     }
 
+    /**
+     * @throws JsonException
+     */
+    #[Route(
+        '/api/ical/search/{query}',
+        name: 'atoolo_events_calendar_ical_search',
+        methods: ['GET'],
+        format: 'json',
+        priority: 2,
+    )]
+    public function iCalBySearch(string $query): Response
+    {
+        $searchQuery = $this->deserializeSearchQuery($query);
+        return $this->createICalResponseBySearchQuery($searchQuery);
+    }
+
+    private function createICalResponseBySearchQuery(SearchQuery $searchQuery): Response
+    {
+        try {
+            $resources = $this->search->search($searchQuery);
+        } catch (\Throwable $th) {
+            $this->logger?->warning(
+                'Something went wrong while exectuing the search query',
+                [
+                    'searchQuery' => $searchQuery,
+                    'exception' => $th,
+                ],
+            );
+            throw new HttpException(500, 'Something went wrong while processing the search query', $th);
+        }
+        return $this->createICalResponeByResources(...$resources);
+    }
+
+    private function deserializeSearchQuery(string $query): SearchQuery
+    {
+        try {
+            return $this->serializer->deserialize(
+                $query,
+                SearchQuery::class,
+                'json',
+            );
+        } catch (\Throwable $th) {
+            $this->logger?->warning(
+                'Something went wrong while trying to deserialize a search query.',
+                [
+                    'serializer' => $this->serializer,
+                    'query' => $query,
+                    'exception' => $th,
+                ],
+            );
+            throw new BadRequestHttpException('Invalid query \'' . $query . '\'', $th);
+        }
+    }
+
     private function createICalResponseByLocation(ResourceLocation $location): Response
     {
         try {
             $resource = $this->loader->load($location);
         } catch (ResourceNotFoundException $e) {
-            throw new NotFoundHttpException('Resource at \'' . $location . '\' not found');
+            throw new NotFoundHttpException('Resource at \'' . $location . '\' not found', $e);
         } catch (InvalidResourceException $e) {
-            throw new HttpException(500, 'Resource at \'' . $location . '\' is invalid');
+            throw new HttpException(500, 'Resource at \'' . $location . '\' is invalid', $e);
         }
         return $this->createICalResponeByResources($resource);
     }
@@ -76,7 +140,11 @@ final class ICalController extends AbstractController
     private function createICalResponeByResources(Resource ...$resources): Response
     {
         $res = new Response($this->iCalFactory->createCalendarAsString(...$resources));
+        $filename = isset($resources[0])
+            ? trim($resources[0]->data->getString('base.title', $resources[0]->name))
+            : 'ical';
         $res->headers->set('Content-Type', 'text/calendar');
+        $res->headers->set('Content-Disposition', 'attachment; filename="' . $filename . '.ics"');
         return $res;
     }
 
